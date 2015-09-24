c3166d3d5234afe708eaf126a7f3d5183f264eed
luolinqiang <luolinqiang@jd.com>
2015-09-24 14:41:08 +0800

    vm

--- jss-index-service/src/main/java/com/jd/jss/index/web/controller/IndexController.java
+++ jss-index-service/src/main/java/com/jd/jss/index/web/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.jd.jss.index.web.controller;
 
 import com.jd.jss.index.web.base.BaseControllor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,7 +21,10 @@ public class IndexController extends BaseControllor {
     }
 
     @RequestMapping(value = "/vm", method = {RequestMethod.GET, RequestMethod.POST}, produces = "application/json;charset=utf-8")
-    public String vm() {
+    public String vm(Model model) {
+        Company company = new Company();
+        company.setAddress("hhhhhhhhhhhh");
+        model.addAttribute("company", company);
         return "testvm";
     }
 
