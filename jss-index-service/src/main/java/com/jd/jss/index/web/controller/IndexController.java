29015f2928d459afef26f4b677a85d5f9787e4e0
bjyfguowenjuan <guowenjuan@jd.com>
2015-09-24 17:43:30 +0800

    guowenjuan 更多页面

--- jss-index-service/src/main/java/com/jd/jss/index/web/controller/IndexController.java
+++ jss-index-service/src/main/java/com/jd/jss/index/web/controller/IndexController.java
@@ -7,17 +7,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author luolinqiang
  */
 @Controller
 public class IndexController extends BaseControllor {
 
-    @RequestMapping(value = "/index", method = {RequestMethod.GET, RequestMethod.POST}, produces = "application/json;charset=utf-8")
-    public String index(Model model) {
+    @RequestMapping(value = "/index")
+    public String index( Model model, HttpServletRequest request) throws Exception {
 
 
         return "index";
+    }
+
+    @RequestMapping(value = "/more")
+    public String more( Model model, HttpServletRequest request) throws Exception {
+
+        return "more";
     }
 
 }
